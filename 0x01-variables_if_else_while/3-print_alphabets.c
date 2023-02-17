32399d62d8f4c6532bf9bbbd0a86db8bebb315b8
Sharonkarwitha <sharonkarwitha@icloud.com>
2023-02-17 15:01:33 -0800

    3 alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,14 +10,17 @@ int main(void)
 	char c = 'a';
 
 	while (c <= 'z')
-{
+	{
 		putchar(c);
 		c++;
 	}
 	c = 'A';
+
 	while (c <= 'Z')
+	{
 	putchar(c);
 	c++;
+	}
+	putchar('\n');
+	return (0);
 }
-putchar('\n');
-return (0);
