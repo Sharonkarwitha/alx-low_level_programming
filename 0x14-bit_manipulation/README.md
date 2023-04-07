6395be46b745be4c371e8cdead3b01f96f4b2094
Sharonkarwitha <sharonkarwitha@icloud.com>
2023-04-07 10:54:48 -0700

    binary

---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+this is a readme file for the bit manipulation project in c
---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * _putchar  - this will put a char to stdout
+ * @c: this is a character to be printed
+ * Return: returns success 
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
