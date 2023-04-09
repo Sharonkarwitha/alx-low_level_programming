1b1d4fb93815b6176b322b50b5ef0bc970107add
Sharonkarwitha <sharonkarwitha@icloud.com>
2023-04-09 13:24:06 -0700

    binary

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- * clear_ bitthis places 0 at specific index
+ * clear_ bit -this places 0 at specific index
  * @n: this is a pointer for altered digits
  * @index : to be cleared
  * Return : returns 1
  */
+
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int h = 1;
-
-	if (sizeof(n) * 8 < index)
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-h <<= index;
-*n &= ~h;
-return (1);
+	*n &= ~(1 << index);
+	return (1);
 }
