000b333d04e5fb6c1afdacfe2e64bcf81c20b465
Sharonkarwitha <sharonkarwitha@icloud.com>
2023-04-09 22:19:33 -0700

    main

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,35 +5,45 @@
 /**
  * read_textfile - this is the entry point
  * @filename:this is the pointer
- * @letters: the size 
+ * @letters: the size
  * Return: the returns
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t file, let, x;
-	char *text;
-
-	text = malloc(letters);
-	if (text == NULL)
-		return (0);
+	int fd;
+	char *buf;
+	ssize_t bytes_read, bytes_written;
 
 	if (filename == NULL)
 		return (0);
 
-	file = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-	if (file == -1)
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
 	{
-		free(text);
+		close(fd);
 		return (0);
 	}
 
-	let = read(file, text, letters);
-
-	x = write(STDOUT_FILENO, text, let);
-
-	close(file);
-
-	return (x);
+	bytes_read = read(fd, buf, letters);
+	if (bytes_read == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+	bytes_written = write(STDOUT_FILENO, buf, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+	free(buf);
+	close(fd);
+	return (bytes_written);
 }
