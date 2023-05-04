a7211f2ed895d1c9d2b683b6921f8e48e289c739
“[Sharonkarwitha]” <[sharonkarwitha@icloud.com]>
2023-05-04 14:37:11 -0700

    crack me

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * binary
+ * @b:character
+ * Return: always return 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i, dee;
+	unsigned int denno;
+
+	i = dee = denno = 0;
+	if (b == NULL)
+		return (0);
+	while (b[dee] != '\0')
+	{
+		if (b[dee] == '0' || b[dee] == '1')
+			dee++;
+		else
+			return (0);
+	}
+	while (i < dee)
+	{
+		denno = denno << 1;
+		if (b[i] == '1')
+			denno += 1;
+		i++;
+	}
+	return (denno);
+}
