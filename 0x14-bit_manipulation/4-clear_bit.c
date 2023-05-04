0c90c3e980bf0077e52993a0624c9706d342612b
“[Sharonkarwitha]” <[sharonkarwitha@icloud.com]>
2023-05-04 15:25:53 -0700

    crack me

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
- * clear_bit - sets value to 0
- * @n:  long i pointer
- * index bit 
- * returns 1 if success.
+ * clear_bit - bit value 0
+ * @n: int pointer
+ * @index: bit 
+ *  Return: always 1
  */
 
 int clear_bit(unsigned long int *n, unsigned int index)
@@ -23,19 +22,6 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	{
 		*n ^= byte;
 	}
-=======
- * clear_ bit -this places 0 at specific index
- * @n: this is a pointer for altered digits
- * @index : to be cleared
- * Return : returns 1
- */
-int clear_bit(unsigned long int *n, unsigned int index)
-{
-	if (index >= sizeof(unsigned long int) * 8)
-		return (-1);
-
-	*n &= ~(1UL << index);
->>>>>>> 7d53686cacc2aeada5359924b9a38de902029725
 
 	return (1);
 }
