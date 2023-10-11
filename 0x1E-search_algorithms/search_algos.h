77724aaf10c95c6921d5c2fd52dda8e56c10fbd6
Sharonkarwitha <sharonkarwitha@icloud.com>
2023-10-11 11:18:18 -0700

    dev

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,18 @@
+#ifndef _search_algos_
+#define _search_algos_
+
+#include <stddef.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t l, size_t r);
+int binary_search_index(int *array, size_t l, size_t r, int value);
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+
+#endif /* _search_algos_ */
